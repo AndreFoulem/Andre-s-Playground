347c4b7bfb479af83f451c1fe4315bad4180f449
AndreFoulem <timmycasual@icloud.com>
2023-04-15 13:45:33 -0700

    Initial Commit

---
+++ Andre's Playground/Andre_s_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Andre_s_PlaygroundApp.swift
+//  Andre's Playground
+//
+//  Created by AndreMacBook on 2023-04-15.
+//
+
+import SwiftUI
+
+@main
+struct Andre_s_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
